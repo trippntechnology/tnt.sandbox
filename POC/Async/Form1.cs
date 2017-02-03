9792917a71b0019d5e582fac402fe280947b3ebd
ppirtnevets <ppirtnevets@e182c406-5238-9a49-a9a2-b1656d343170>
2017-02-03 03:36:21 +0000

    Add Async. Update PDF reference to iTextSharp.

---
+++ POC/Async/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			tb.AppendText("Button clicked");
+			await WaitAsynchronouslyAsync();
+		}
+
+		// The following method runs asynchronously. The UI thread is not
+		// blocked during the delay. You can move or resize the Form1 window 
+		// while Task.Delay is running.
+		public async Task<string> WaitAsynchronouslyAsync()
+		{
+			tb.AppendText("\r\nWaiting 10 seconds ...");
+			await Task.Delay(10000);
+			tb.AppendText("done");
+			return "Finished";
+		}
+
+		// The following method runs synchronously, despite the use of async.
+		// You cannot move or resize the Form1 window while Thread.Sleep
+		// is running because the UI thread is blocked.
+		public string WaitSynchronously()
+		{
+			// Add a using directive for System.Threading.
+			Thread.Sleep(10000);
+			return "Finished";
+		}
+	}
+}
