2dd83c9f51c2f3e964c0270568da68a439373887
ppirtnevets <ppirtnevets@e182c406-5238-9a49-a9a2-b1656d343170>
2017-06-15 18:48:47 +0000

    Add VersionInfo

---
+++ VersionInfo/AssemblyInformation.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Reflection;
+using TNT.Utilities;
+
+namespace VersionInfo
+{
+	public class AssemblyInformation
+	{
+		public string AssemblyName { get; set; }
+
+		public string Product { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string FileVersion { get; set; }
+
+		public string InformationalVersion { get; set; }
+
+		public AssemblyInformation(string assemblyPath)
+		{
+			Assembly assembly = Assembly.LoadFile(assemblyPath);
+
+			this.AssemblyName = Path.GetFileName(assemblyPath);
+			this.Description = Utilities.GetAssemblyAttribute<AssemblyDescriptionAttribute>(assembly)?.Description;
+			this.FileVersion = Utilities.GetAssemblyAttribute<AssemblyFileVersionAttribute>(assembly)?.Version;
+			this.InformationalVersion = Utilities.GetAssemblyAttribute<AssemblyInformationalVersionAttribute>(assembly)?.InformationalVersion;
+			this.Title = Utilities.GetAssemblyAttribute<AssemblyTitleAttribute>(assembly)?.Title;
+			this.Product = Utilities.GetAssemblyAttribute<AssemblyProductAttribute>(assembly)?.Product;
+		}
+
+	}
+}
---
+++ VersionInfo/Parameters.cs
@@ -0,0 +1,30 @@
+﻿using TNT.Utilities;
+using TNT.Utilities.Console;
+
+namespace VersionInfo
+{
+	public class Parameters : TNT.Utilities.Console.Parameters
+	{
+		private static string ENV = "e";
+
+		private Environment _Environment = null;
+
+		public Environment Environment
+		{
+			get
+			{
+				if (_Environment == null)
+				{
+					_Environment = Utilities.DeserializeFromFile<Environment>((this[ENV] as FileParameter).Value);
+				}
+
+				return _Environment;
+			}
+		}
+
+		public Parameters()
+		{
+			this.Add(new FileParameter(ENV, "File containing the evironent information", true) { MustExist = true });
+		}
+	}
+}
