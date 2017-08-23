689500273573c67c25ad55f5884845b1f68e0107
ppirtnevets <ppirtnevets@e182c406-5238-9a49-a9a2-b1656d343170>
2017-08-23 19:44:37 +0000

    Add

---
+++ POC/OAuth/OAuth2Server/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services.InMemory;
+
+namespace OAuth2Server
+{
+	class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			var options = new IdentityServerOptions
+			{
+				Factory = new IdentityServerServiceFactory()
+											.UseInMemoryClients(Clients.Get())
+											.UseInMemoryScopes(Scopes.Get())
+											.UseInMemoryUsers(Users.Get()),
+
+				RequireSsl = false
+			};
+
+			app.UseIdentityServer(options);
+		}
+	}
+}
