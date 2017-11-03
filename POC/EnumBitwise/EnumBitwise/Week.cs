08f1e7bfd98990800b1deecd76bedf7f05fde4d1
ppirtnevets <ppirtnevets@e182c406-5238-9a49-a9a2-b1656d343170>
2017-11-03 01:30:07 +0000

    Add missing files.

---
+++ POC/EnumBitwise/EnumBitwise/Week.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumBitwise
+{
+	[Flags]
+	enum Week
+	{
+		Sunday = 1,
+		Monday = 2,
+		Tuesday = 4,
+		Wedndesday = 8,
+		Thursday = 16,
+		Friday = 32,
+		Saturday = 64
+	}
+}
