bbeb81715ddea8b4c3371fd32014b185087ecbd7
ppirtnevets <ppirtnevets@e182c406-5238-9a49-a9a2-b1656d343170>
2017-11-03 01:30:07 +0000

    Add missing files.

---
+++ EnumBitwise/EnumBitwise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumBitwise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Week week1 = Week.Monday | Week.Wedndesday | Week.Friday;
+
+			bool result1 = week1.HasFlag(Week.Wedndesday);
+		}
+	}
+}
