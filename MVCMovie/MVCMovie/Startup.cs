faf137577b672d2ce4c40516f97f48ec09da7789
Steve Tripp <stevetripp@comcast.net>
2018-11-12 19:44:18 -0700

    Migrate to Sqlite

--- MVCMovie/MVCMovie/Startup.cs
+++ MVCMovie/MVCMovie/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using MVCMovie.Models;
 
 namespace MVCMovie
@@ -27,16 +23,17 @@ namespace MVCMovie
 		{
 			services.Configure<CookiePolicyOptions>(options =>
 			{
-							// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-							options.CheckConsentNeeded = context => true;
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
 
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-		    services.AddDbContext<MVCMovieContext>(options =>
-		            options.UseSqlServer(Configuration.GetConnectionString("MVCMovieContext")));
+			services.AddDbContext<MVCMovieContext>(options =>
+				options.UseSqlite("Data Source=MvcMovie.sqlite"));
+			//options.UseSqlServer(Configuration.GetConnectionString("MVCMovieContext")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
