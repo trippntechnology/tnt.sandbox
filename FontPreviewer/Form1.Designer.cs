dca81c8af53bc5188a5c833a31cb08be33ea90ba
Steve <stevetripp@comcast.net>
2019-02-18 12:57:11 -0700

    Add FontPreviewer

---
+++ FontPreviewer/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FontPreviewer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richTextBox = new System.Windows.Forms.RichTextBox();
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// richTextBox
+			// 
+			this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox.Location = new System.Drawing.Point(0, 74);
+			this.richTextBox.Name = "richTextBox";
+			this.richTextBox.Size = new System.Drawing.Size(800, 681);
+			this.richTextBox.TabIndex = 0;
+			this.richTextBox.Text = "";
+			this.richTextBox.WordWrap = false;
+			// 
+			// textBox
+			// 
+			this.textBox.Dock = System.Windows.Forms.DockStyle.Top;
+			this.textBox.Location = new System.Drawing.Point(0, 0);
+			this.textBox.Multiline = true;
+			this.textBox.Name = "textBox";
+			this.textBox.Size = new System.Drawing.Size(800, 74);
+			this.textBox.TabIndex = 1;
+			this.textBox.WordWrap = false;
+			this.textBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(713, 45);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 755);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.richTextBox);
+			this.Controls.Add(this.textBox);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox;
+		private System.Windows.Forms.TextBox textBox;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ FontPreviewer/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FontPreviewer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void textBox1_KeyUp(object sender, KeyEventArgs e)
+		{
+
+		}
+
+		private void PreviewFonts()
+		{
+			var fonts = new List<FontFamily>();
+
+			foreach (FontFamily font in System.Drawing.FontFamily.Families)
+			{
+				fonts.Add(font);
+			}
+			richTextBox.Clear();
+
+			foreach (var fontFamily in fonts)
+			////for (var i = 0; i < 5; i++)
+			{
+				//var fontFamily = fonts[i];
+				richTextBox.SelectionFont = new Font(fontFamily, 20f);
+				richTextBox.AppendText($"{fontFamily.Name}\n\t{textBox.Text}\n");
+			}
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			PreviewFonts();
+		}
+	}
+}
