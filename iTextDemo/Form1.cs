5a7be7cb062a09d911c429e36464fe00c44884de
Steve Tripp <steve@trippntechnology.com>
2024-05-25 10:17:45 -0600

    Add addImage()

--- iTextDemo/Form1.cs
+++ iTextDemo/Form1.cs
@@ -1,8 +1,11 @@
 
+using iText.IO.Image;
 using iText.Kernel.Events;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
+using iText.Layout.Properties;
+using iTextImage = iText.Layout.Element.Image;
 
 namespace iTextDemo;
 
@@ -39,10 +42,34 @@ public partial class Form1 : Form
           // Add content to the document
           document.Add(new Paragraph("Hello, iText!"));
 
+          addImage(document);
+
           // Add content to the document
           document.Add(new Paragraph("This is some sample content."));
         }
       }
     }
+  }
+
+  private void addImage(Document document, string imagePath = "70_80.jpg")
+  {
+    // Create text elements
+    Text beforeImageText = new Text("This is some text before the image. ");
+    Text afterImageText = new Text(" This is some text after the image.");
+
+    // Load image
+    ImageData imageData = ImageDataFactory.Create(imagePath);
+    iTextImage image = new iTextImage(imageData);
+    image.SetWidth(200 * image.GetImageWidth() / image.GetImageHeight());
+    image.SetHeight(200);
+
+    // Create a paragraph and add text and image inline
+    Paragraph paragraph = new Paragraph()
+        .Add(beforeImageText)
+        .Add(image)
+        .Add(afterImageText);
+
+    // Add paragraph to the document
+    document.Add(paragraph);
   }
 }
