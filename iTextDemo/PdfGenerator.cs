affb12949ec5609a79b8b27b3eefa65484005eae
Steve Tripp <steve@trippntechnology.com>
2024-11-30 12:01:38 -0700

    Display single page instead of continuous scrolling.

--- iTextDemo/PdfGenerator.cs
+++ iTextDemo/PdfGenerator.cs
@@ -7,15 +7,13 @@ internal class PdfGenerator
 {
   public virtual void Generate(string filename, Action<PdfDocument, Document> onGenerate)
   {
-    using (PdfWriter pdfWriter = new PdfWriter(filename))
-    {
-      using (PdfDocument pdfDocument = new PdfDocument(pdfWriter))
-      {
-        using (Document document = new Document(pdfDocument))
-        {
-          onGenerate(pdfDocument, document);
-        }
-      }
-    }
+    using PdfWriter pdfWriter = new PdfWriter(filename);
+    using PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+    using Document document = new Document(pdfDocument);
+
+    // Display one page at a time rather than continuous scrolling.
+    pdfDocument.GetCatalog().SetPageLayout(PdfName.SinglePage);
+
+    onGenerate(pdfDocument, document);
   }
 }
