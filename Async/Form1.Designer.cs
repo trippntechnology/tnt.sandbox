371f07a3221af56f269710419b72340d4d334b50
Steve Tripp <steve@trippntechnology.com>
2025-01-24 09:51:25 -0700

    Update .NET 9

--- Async/Form1.Designer.cs
+++ Async/Form1.Designer.cs
@@ -66,7 +66,7 @@
 			this.textBox1.Name = "textBox1";
 			this.textBox1.Size = new System.Drawing.Size(247, 20);
 			this.textBox1.TabIndex = 3;
-			this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUpAsync);
+			this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
 			// 
 			// Form1
 			// 
--- Async/Form1.cs
+++ Async/Form1.cs
@@ -1,186 +1,179 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Diagnostics;
 
 namespace Async
 {
-	public partial class Form1 : Form
-	{
-		private const string Category = "Debouncer";
-		CancellationTokenSource cts = null;
-		object _Lock = new object();
+  public partial class Form1 : Form
+  {
+    private const string Category = "Debouncer";
+    CancellationTokenSource? cts = null;
+    object _Lock = new object();
 
-		public Form1()
-		{
-			InitializeComponent();
-		}
+    public Form1()
+    {
+      InitializeComponent();
+    }
 
-		private async void button1_ClickAsync(object sender, EventArgs e)
-		{
-			tb.AppendText("Button clicked\n");
-			WaitAsynchronouslyAsync();
-			await WaitAsynchronouslyAsync();
-			tb.AppendText($"WaitAsynchronouslyAsync returned\n");// ({result})");
-																													 //var result = await CallThread();
-																													 //listBox1.Items.Add($"ThreadCall {result}");
-		}
+    private async void button1_ClickAsync(object sender, EventArgs e)
+    {
+      tb.AppendText("Button clicked\n");
+      await WaitAsynchronouslyAsync();
+      tb.AppendText($"WaitAsynchronouslyAsync returned\n");// ({result})");
+                                                           //var result = await CallThread();
+                                                           //listBox1.Items.Add($"ThreadCall {result}");
+    }
 
-		// The following method runs asynchronously. The UI thread is not
-		// blocked during the delay. You can move or resize the Form1 window 
-		// while Task.Delay is running.
-		public async Task WaitAsynchronouslyAsync()
-		{
-			IProgress<string> progress = new Progress<string>(text =>
-		 {
-			 try
-			 {
-				 tb.AppendText($"{text}\n");
-			 }
-			 catch { }
-		 });
+    // The following method runs asynchronously. The UI thread is not
+    // blocked during the delay. You can move or resize the Form1 window 
+    // while Task.Delay is running.
+    public async Task WaitAsynchronouslyAsync()
+    {
+      IProgress<string> progress = new Progress<string>(text =>
+     {
+       try
+       {
+         tb.AppendText($"{text}\n");
+       }
+       catch { }
+     });
 
-			if (cts != null)
-			{
-				cts.Cancel();
-			}
+      if (cts != null)
+      {
+        cts.Cancel();
+      }
 
-			cts = new CancellationTokenSource();
+      cts = new CancellationTokenSource();
 
-			await Task.Run(() =>
-			{
-				var token = cts.Token;
-				for (var i = 0; i < 1000 && !token.IsCancellationRequested; i++)
-				{
-					progress.Report(i.ToString());
-					Thread.Sleep(10);
-				}
-				progress.Report("done");
-			}, cts.Token);
-		}
+      await Task.Run(() =>
+      {
+        var token = cts.Token;
+        for (var i = 0; i < 1000 && !token.IsCancellationRequested; i++)
+        {
+          progress.Report(i.ToString());
+          Thread.Sleep(10);
+        }
+        progress.Report("done");
+      }, cts.Token);
+    }
 
-		// The following method runs synchronously, despite the use of async.
-		// You cannot move or resize the Form1 window while Thread.Sleep
-		// is running because the UI thread is blocked.
-		public string WaitSynchronously()
-		{
-			// Add a using directive for System.Threading.
-			Thread.Sleep(10000);
-			return "Finished";
-		}
+    // The following method runs synchronously, despite the use of async.
+    // You cannot move or resize the Form1 window while Thread.Sleep
+    // is running because the UI thread is blocked.
+    public string WaitSynchronously()
+    {
+      // Add a using directive for System.Threading.
+      Thread.Sleep(10000);
+      return "Finished";
+    }
 
-		public async Task<string> CallThread()
-		{
-			Debug.WriteLine("CallThread() called");
+    public string CallThread()
+    {
+      Debug.WriteLine("CallThread() called");
 
-			var result = Task.Run<string>(() =>
-			{
-				Debug.WriteLine("Task started");
-				Thread.Sleep(5000);
-				return "done";
-			});
+      var result = Task.Run<string>(() =>
+      {
+        Debug.WriteLine("Task started");
+        Thread.Sleep(5000);
+        return "done";
+      });
 
 
-			Debug.WriteLine("Waiting for task to complete");
-			result.Wait();
-			Debug.WriteLine("Returning from CallThread()");
-			return result.Result;
-		}
+      Debug.WriteLine("Waiting for task to complete");
+      result.Wait();
+      Debug.WriteLine("Returning from CallThread()");
+      return result.Result;
+    }
 
-		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-		{
-			cts.Cancel();
-		}
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      cts?.Cancel();
+    }
 
-		private async void textBox1_KeyUpAsync(object sender, KeyEventArgs e)
-		{
-			Debug.WriteLine("Called", "Debounce");
+    private void textBox1_KeyUp(object sender, KeyEventArgs e)
+    {
+      Debug.WriteLine("Called", "Debounce");
 
-			IProgress<string> progress = new Progress<string>(text =>
-			{
-				try
-				{
-					tb.AppendText($"{text}\n");
-				}
-				catch { }
-			});
+      IProgress<string> progress = new Progress<string>(text =>
+      {
+        try
+        {
+          tb.AppendText($"{text}\n");
+        }
+        catch { }
+      });
 
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-			Debounce((sender as TextBox).Text, (text, token) =>
-				{
-					if (token.IsCancellationRequested)
-					{
-						Debug.WriteLine("Token was cancelled", Category);
-					}
-					else
-					{
-						Debug.WriteLine("Doing Work", Category);
-						progress.Report(text);
-					}
-				});
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-		}
+      Debounce((sender as TextBox)!.Text, (text, token) =>
+        {
+          if (token.IsCancellationRequested)
+          {
+            Debug.WriteLine("Token was cancelled", Category);
+          }
+          else
+          {
+            Debug.WriteLine("Doing Work", Category);
+            progress.Report(text);
+          }
+        });
+    }
 
-		private async Task Debounce(string text, Action<string, CancellationToken> action)
-		{
-			if (cts != null)
-			{
-				Debug.Write("Canceling previous task", Category);
-				cts.Cancel();
-				Debug.WriteLine(" ... Done");
-				//cts = null;
-			}
+    private void Debounce(string text, Action<string, CancellationToken> action)
+    {
+      if (cts != null)
+      {
+        Debug.Write("Canceling previous task", Category);
+        cts.Cancel();
+        Debug.WriteLine(" ... Done");
+        //cts = null;
+      }
 
-			while (cts?.IsCancellationRequested == false)
-			{
-				Debug.WriteLine("Waiting for task to cancel", Category);
-			}
+      while (cts?.IsCancellationRequested == false)
+      {
+        Debug.WriteLine("Waiting for task to cancel", Category);
+      }
 
-			//lock (_Lock)
-			//{
-				cts = new CancellationTokenSource();
+      //lock (_Lock)
+      //{
+      cts = new CancellationTokenSource();
 
-				//try
-				//{
-				var task = Task.Run(async () =>
-				{
-					try
-					{
-						Debug.WriteLine("Entered Run", Category);
+      //try
+      //{
+      var task = Task.Run(async () =>
+      {
+        try
+        {
+          Debug.WriteLine("Entered Run", Category);
 
-						//try
-						//{
-						await Task.Delay(1000, cts.Token);
-						//}
-						//catch (Exception ex)
-						//{
-						//	Debug.WriteLine($"Delay {ex.Message}", Category);
-						//}
+          //try
+          //{
+          await Task.Delay(1000, cts.Token);
+          //}
+          //catch (Exception ex)
+          //{
+          //	Debug.WriteLine($"Delay {ex.Message}", Category);
+          //}
 
-						if (!cts.Token.IsCancellationRequested)
-						{
-							action(text, cts.Token);
-						}
-						else
-						{
-							Debug.WriteLine("Token cancelled", Category);
-							cts.Token.ThrowIfCancellationRequested();
-						}
+          if (!cts.Token.IsCancellationRequested)
+          {
+            action(text, cts.Token);
+          }
+          else
+          {
+            Debug.WriteLine("Token cancelled", Category);
+            cts.Token.ThrowIfCancellationRequested();
+          }
 
-						Debug.WriteLine("Task complete", Category);
-					}
-					catch (Exception ex)
-					{
-						Debug.WriteLine($"Exception: {ex.Message}", Category);
-					}
-				}, cts.Token);
-				//}
-				//catch (Exception ex)
-				//{
-				//	Debug.WriteLine($"Do Work {ex.Message}", Category);
-				//}
-			//}
-		}
-	}
+          Debug.WriteLine("Task complete", Category);
+        }
+        catch (Exception ex)
+        {
+          Debug.WriteLine($"Exception: {ex.Message}", Category);
+        }
+      }, cts.Token);
+      //}
+      //catch (Exception ex)
+      //{
+      //	Debug.WriteLine($"Do Work {ex.Message}", Category);
+      //}
+      //}
+    }
+  }
 }
